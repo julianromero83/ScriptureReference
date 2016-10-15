ffc14e4812dcf83b0df21c3ffe1b9a029bebe4a4
julianromero83 <rom16015@byui.edu>
2016-10-15 11:48:14 -0700

    Initial commit

---
+++ app/src/main/java/edu/byui/www/scripturereference/ScriptureReference.java
@@ -0,0 +1,13 @@
+package edu.byui.www.scripturereference;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScriptureReference extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scripture_reference);
+    }
+}
