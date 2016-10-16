79c3718636354eb588519723571ade084d4bd34e
julianromero83 <julianromero83@gmail.com>
2016-10-15 23:04:42 -0700

    Created second activity to display favorite scripture string.

--- app/src/main/java/edu/byui/www/scripturereference/ScriptureReference.java
+++ app/src/main/java/edu/byui/www/scripturereference/ScriptureReference.java
@@ -1,13 +1,41 @@
 package edu.byui.www.scripturereference;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ScriptureReference extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scripture_reference);
+
+        Button submit = (Button)findViewById(R.id.buttonSubmit);
+        final EditText scriptBook = (EditText)findViewById(R.id.scriptureBook);
+        final EditText scriptChapter = (EditText)findViewById(R.id.chapter);
+        final EditText scriptVerse = (EditText)findViewById(R.id.verse);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent favorite = new Intent(ScriptureReference.this, FavoriteScripture.class);
+                String bk = scriptBook.getText().toString();
+                String ch = scriptChapter.getText().toString();
+                String vs = scriptVerse.getText().toString();
+                favorite.putExtra("book", bk);
+                favorite.putExtra("chapter", ch);
+                favorite.putExtra("verse", vs);
+                startActivity(favorite);
+            }
+        });
+
     }
 }
